bc11d7c02df3cca21eb3bc577bb2a913374df8d1
Jameem <jameemkurikkal.mp@gmail.com>
2020-11-19 08:29:42 +0530

    refractored error handler middleware

--- auth/src/middlewares/errorHandler.ts
+++ auth/src/middlewares/errorHandler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express"
-import { RequestValidationError } from "../errors/requestValidationError"
-import { DatabaseConnectionError } from "../errors/databaseConnectionError"
+import { CustomError } from "../errors/customError"
 
 export const errorHandler = (
   err: Error,
@@ -8,14 +7,8 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() })
-  }
-
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(503).send({
-      errors: err.serializeErrors(),
-    })
   }
 
   res.status(400).send({
