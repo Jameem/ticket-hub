c46eedaf057420ec27dc02b6c2281ea7da1842c0
Jameem <jameemkurikkal.mp@gmail.com>
2020-11-19 08:29:18 +0530

    update existing error classes

--- auth/src/errors/databaseConnectionError.ts
+++ auth/src/errors/databaseConnectionError.ts
@@ -1,9 +1,11 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from "./customError"
+
+export class DatabaseConnectionError extends CustomError {
   statusCode = 503
   reason = "Error connecting to database!"
 
   constructor() {
-    super()
+    super("Error connecting to database!")
 
     // Only because of we are extending a built in Class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype)
--- auth/src/errors/requestValidationError.ts
+++ auth/src/errors/requestValidationError.ts
@@ -1,10 +1,11 @@
 import { ValidationError } from "express-validator"
+import { CustomError } from "./customError"
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
   statusCode = 400
 
   constructor(public errors: ValidationError[]) {
-    super()
+    super("Request Validation Error")
 
     // Only because of we are extending a built in Class
     Object.setPrototypeOf(this, RequestValidationError.prototype)
