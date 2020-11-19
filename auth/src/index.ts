fb22c2d514663c3bae5ff75fb2786df46b67d7e4
Jameem <jameemkurikkal.mp@gmail.com>
2020-11-19 09:02:08 +0530

    wire up mongodb connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import "express-async-errors"
+import mongoose from "mongoose"
 
 import { currentUserRouter } from "./routes/currentUser"
 import { signinRouter } from "./routes/signin"
@@ -22,6 +23,22 @@ app.all("*", async () => {
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log(`Listening on port 3000!`)
-})
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+  } catch (err) {
+    console.error(err)
+  }
+
+  console.table("Connected to the database..")
+
+  app.listen(3000, () => {
+    console.log(`Listening on port 3000!`)
+  })
+}
+
+start()
